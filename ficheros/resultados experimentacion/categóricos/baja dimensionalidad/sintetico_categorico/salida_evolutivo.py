03ee3aa59ff303ccaba198fa26573690e8e5de3e
Nemesio Romero Pino i82ropin <71700136+whitenrp@users.noreply.github.com>
2025-09-10 19:26:15 +0200

    Add files via upload

---
+++ ficheros/resultados experimentacion/categóricos/baja dimensionalidad/sintetico_categorico/salida_evolutivo.py
@@ -0,0 +1,25 @@
+Existen 12 relaciones:
+[('X0', 'X1'), ('X0', 'X3'), ('X0', 'X8'), ('X1', 'X3'), ('X1', 'X4'), ('X1', 'X9'), ('X2', 'X3'), ('X3', 'X9'), ('X4', 'X12'), ('X5', 'X19'), ('X6', 'X9'), ('X9', 'X12')]
+Vecinos:
+El tiempo de tu algoritmo es 178.30
+Uso de CPU: 38.80%
+Memoria utilizada (actual): 0.36 MB
+Memoria pico: 39.04 MB
+
+
+Existen 12 relaciones:
+[('X0', 'X1'), ('X0', 'X3'), ('X0', 'X8'), ('X1', 'X3'), ('X1', 'X4'), ('X1', 'X9'), ('X2', 'X3'), ('X3', 'X9'), ('X4', 'X12'), ('X5', 'X19'), ('X6', 'X9'), ('X9', 'X12')]
+Vecinos:
+El tiempo de tu algoritmo es 168.30
+Uso de CPU: 35.80%
+Memoria utilizada (actual): 0.36 MB
+Memoria pico: 41.64 MB
+
+
+Existen 12 relaciones:
+[('X0', 'X1'), ('X0', 'X3'), ('X0', 'X8'), ('X1', 'X3'), ('X1', 'X4'), ('X1', 'X9'), ('X2', 'X3'), ('X3', 'X9'), ('X4', 'X12'), ('X5', 'X19'), ('X6', 'X9'), ('X9', 'X12')]
+Vecinos:
+El tiempo de tu algoritmo es 176.30
+Uso de CPU: 25.80%
+Memoria utilizada (actual): 0.36 MB
+Memoria pico: 36.44 MB
---
+++ ficheros/resultados experimentacion/categóricos/baja dimensionalidad/sintetico_categorico/salida_pc-stable.py
@@ -0,0 +1,25 @@
+PC-stable Ha detectado 15 
+Las aristas aprendidas por PC son:
+[('X0', 'X3'), ('X0', 'X8'), ('X0', 'X1'), ('X2', 'X3'), ('X4', 'X12'), ('X4', 'X1'), ('X1', 'X3'), ('X1', 'X9'), ('X1', 'X0'), ('X1', 'X4'), ('X9', 'X12'), ('X6', 'X9'), ('X8', 'X0'), ('X19', 'X5'), ('X5', 'X19')]
+El tiempo de PC-stable es 23.43
+Uso de CPU: 12.30%
+Memoria utilizada (actual): 15.47 MB
+Memoria pico: 36.66 MB
+
+
+PC-stable Ha detectado 15 
+Las aristas aprendidas por PC son:
+[('X0', 'X3'), ('X0', 'X8'), ('X0', 'X1'), ('X2', 'X3'), ('X4', 'X12'), ('X4', 'X1'), ('X1', 'X3'), ('X1', 'X9'), ('X1', 'X0'), ('X1', 'X4'), ('X9', 'X12'), ('X6', 'X9'), ('X8', 'X0'), ('X19', 'X5'), ('X5', 'X19')]
+El tiempo de PC-stable es 24.79
+Uso de CPU: 13.30%
+Memoria utilizada (actual): 15.47 MB
+Memoria pico: 36.46 MB
+
+
+PC-stable Ha detectado 15 
+Las aristas aprendidas por PC son:
+[('X0', 'X3'), ('X0', 'X8'), ('X0', 'X1'), ('X2', 'X3'), ('X4', 'X12'), ('X4', 'X1'), ('X1', 'X3'), ('X1', 'X9'), ('X1', 'X0'), ('X1', 'X4'), ('X9', 'X12'), ('X6', 'X9'), ('X8', 'X0'), ('X19', 'X5'), ('X5', 'X19')]
+El tiempo de PC-stable es 25.69
+Uso de CPU: 10.30%
+Memoria utilizada (actual): 15.47 MB
+Memoria pico: 39.46 MB
